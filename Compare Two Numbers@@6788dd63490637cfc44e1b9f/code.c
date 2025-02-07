455e5784f4cd62e70df665e740b45c6015cf98b5
BS-BHOOMISHARMA <bhoomi0199.becse24@chitkara.edu.in>
2025-02-07 14:13:57 +0530

    Solution to Compare Two Numbers

--- Compare Two Numbers@@6788dd63490637cfc44e1b9f/code.c
+++ Compare Two Numbers@@6788dd63490637cfc44e1b9f/code.c
@@ -3,12 +3,15 @@ int main()
 {
 int a,b;
 scanf("%d%d",&a,&b);
-if (a>b)
-{
-    printf("First");
-    else
-    printf("Second");
-}
-else
-printf("Equal");
+if (a > b) {
+        printf("First\n");
+    } 
+    else if (a < b) {
+        printf("Second\n");
+    } 
+    else {
+        printf("Equal\n");
+    }
+
+    return 0;
 }
