f866f3b02922502920a251d63c7df87327ddfb28
BS-BHOOMISHARMA <bhoomi0199.becse24@chitkara.edu.in>
2025-03-29 13:06:54 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

---
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int n,m;
+    scanf("%d",n);
+    int r[];
+    for(int i=0;i<n;i++)
+    scanf("%d",&r[i]);
+    scanf("%d",&m);
+    int p[];
+    for(int i=0;i<m;i++)
+    scanf("%d",&player[]);
+    int rs[];
+    
+    
+    }
