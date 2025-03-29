40385dffce57b75eb42c0e1134554b642f32dcb4
BS-BHOOMISHARMA <bhoomi0199.becse24@chitkara.edu.in>
2025-03-29 13:17:32 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
-int main(){
-    int n,m;
-    scanf("%d",n);
-    int r[];
-    for(int i=0;i<n;i++)
-    scanf("%d",&r[i]);
-    scanf("%d",&m);
-    int p[];
-    for(int i=0;i<m;i++)
-    scanf("%d",&player[]);
-    int rs[];
-    
-    
+
+int main() {
+    int n, m;
+    // Read leaderboard size
+    scanf("%d", &n);
+    int r[n]; // Define array with size n
+    // Read leaderboard scores
+    for (int i = 0; i < n; i++)
+        scanf("%d", &r[i]);
+    // Read number of games played
+    scanf("%d", &m);
+    int p[m]; // Define array with size m
+    // Read player scores
+    for (int i = 0; i < m; i++)
+        scanf("%d", &p[i]);
+
+    int rs[m]; // Array to store results
+
+    // Process leaderboard ranking
+    int rank = 1, j = 0;
+    for (int i = 1; i < n; i++) {
+        if (r[i] != r[i - 1]) 
+            rank++;
     }
+
+    int pos = rank; // Start from lowest rank
+
+    // Find ranks for player scores
+    for (int i = 0; i < m; i++) {
+        while (j < n && p[i] >= r[j]) 
+            j++;
+        rs[i] = j + 1;
+    }
+
+    // Print the results
+    for (int i = 0; i < m; i++)
+        printf("%d\n", rs[i]);
+
+    return 0;
+}
